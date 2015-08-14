670283e1d856944ed8814c0ab7e3885e173358fa
Dylan <invision33@gmail.com>
2015-08-14 16:56:11 -0500

    sessions are working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,24 @@ class SessionsController < ApplicationController
   #get
   def login
   end
+
   #post
   def confirmation
   	#show message regarding login status 
+  	@user = User.find_by_email(params[:email])
+
+  	if @user && @user.authenticate(params[:password]) #working with has_secure_password in model
+  		session[:user_id] = @user.user_id
+  		redirect_to '/'
+  	else 
+  		@message = 'User account or password invalid'
+  		render '/sessions/login'
+  	end 
   end
+
   #get
   def logout
+  	sessions[:user_id] = nil
+  	redirect_to '/users/login'
   end
 end
