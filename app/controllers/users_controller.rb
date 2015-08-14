cc4095aa2727b7dd5085068bac72ad871cf09692
Dylan <invision33@gmail.com>
2015-08-14 12:31:47 -0500

    add controller and authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,30 @@
 class UsersController < ApplicationController
   # get 
   def signup
-  	#render view and let user register
+  	#render view (form) and let user register
+
   end
 
   # post 
   def confirmation
-  	# post request that handles signing up 
+  	# # post request that handles signing up 
+  	# puts '-----users#confirmation'
+  	# puts user_params 
+ 		# puts '--------'
+ 		@user = User.new(user_params)
+ 		if @user.save
+
+ 			session[:user_id] = @user.id 
+ 		else 
+ 			@message = 'User account exists or passwords do not match'
+ 			render 'users/signup'
+
+ 		end 
   end
+
+  private 
+
+  	def user_params
+  		params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  	end 
 end
